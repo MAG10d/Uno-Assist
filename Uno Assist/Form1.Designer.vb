e6c4f3a5ff1ce373f4e56cbbb270495448b832ef
Frodwo <qianshangfangbaizi@gmail.com>
2022-10-05 17:52:40 +0800

    加入專案檔案。

---
+++ Uno Assist/Form1.Designer.vb
@@ -0,0 +1,263 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmUnoAssist
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.Button16 = New System.Windows.Forms.Button()
+        Me.Button17 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button1.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button1.Location = New System.Drawing.Point(116, 21)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(50, 50)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.LightYellow
+        Me.Button2.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button2.Location = New System.Drawing.Point(163, 21)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(50, 50)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.BackColor = System.Drawing.Color.LightCyan
+        Me.Button3.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button3.Location = New System.Drawing.Point(116, 68)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(50, 50)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = False
+        '
+        'Button4
+        '
+        Me.Button4.BackColor = System.Drawing.Color.Honeydew
+        Me.Button4.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button4.Location = New System.Drawing.Point(163, 68)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(50, 50)
+        Me.Button4.TabIndex = 3
+        Me.Button4.UseVisualStyleBackColor = False
+        '
+        'Button5
+        '
+        Me.Button5.BackColor = System.Drawing.Color.Honeydew
+        Me.Button5.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button5.Location = New System.Drawing.Point(257, 162)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(50, 50)
+        Me.Button5.TabIndex = 7
+        Me.Button5.UseVisualStyleBackColor = False
+        '
+        'Button6
+        '
+        Me.Button6.BackColor = System.Drawing.Color.LightCyan
+        Me.Button6.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button6.Location = New System.Drawing.Point(210, 162)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(50, 50)
+        Me.Button6.TabIndex = 6
+        Me.Button6.UseVisualStyleBackColor = False
+        '
+        'Button7
+        '
+        Me.Button7.BackColor = System.Drawing.Color.LightYellow
+        Me.Button7.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button7.Location = New System.Drawing.Point(257, 115)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(50, 50)
+        Me.Button7.TabIndex = 5
+        Me.Button7.UseVisualStyleBackColor = False
+        '
+        'Button8
+        '
+        Me.Button8.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button8.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button8.Location = New System.Drawing.Point(210, 115)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(50, 50)
+        Me.Button8.TabIndex = 4
+        Me.Button8.UseVisualStyleBackColor = False
+        '
+        'Button9
+        '
+        Me.Button9.BackColor = System.Drawing.Color.Honeydew
+        Me.Button9.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button9.Location = New System.Drawing.Point(68, 162)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(50, 50)
+        Me.Button9.TabIndex = 11
+        Me.Button9.UseVisualStyleBackColor = False
+        '
+        'Button10
+        '
+        Me.Button10.BackColor = System.Drawing.Color.LightCyan
+        Me.Button10.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button10.Location = New System.Drawing.Point(21, 162)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(50, 50)
+        Me.Button10.TabIndex = 10
+        Me.Button10.UseVisualStyleBackColor = False
+        '
+        'Button11
+        '
+        Me.Button11.BackColor = System.Drawing.Color.LightYellow
+        Me.Button11.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button11.Location = New System.Drawing.Point(68, 115)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(50, 50)
+        Me.Button11.TabIndex = 9
+        Me.Button11.UseVisualStyleBackColor = False
+        '
+        'Button12
+        '
+        Me.Button12.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button12.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button12.Location = New System.Drawing.Point(21, 115)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(50, 50)
+        Me.Button12.TabIndex = 8
+        Me.Button12.UseVisualStyleBackColor = False
+        '
+        'Button13
+        '
+        Me.Button13.BackColor = System.Drawing.Color.Honeydew
+        Me.Button13.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button13.Location = New System.Drawing.Point(163, 258)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(50, 50)
+        Me.Button13.TabIndex = 15
+        Me.Button13.UseVisualStyleBackColor = False
+        '
+        'Button14
+        '
+        Me.Button14.BackColor = System.Drawing.Color.LightCyan
+        Me.Button14.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button14.Location = New System.Drawing.Point(116, 258)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(50, 50)
+        Me.Button14.TabIndex = 14
+        Me.Button14.UseVisualStyleBackColor = False
+        '
+        'Button15
+        '
+        Me.Button15.BackColor = System.Drawing.Color.LightYellow
+        Me.Button15.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button15.Location = New System.Drawing.Point(163, 211)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(50, 50)
+        Me.Button15.TabIndex = 13
+        Me.Button15.UseVisualStyleBackColor = False
+        '
+        'Button16
+        '
+        Me.Button16.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button16.Font = New System.Drawing.Font("Microsoft JhengHei UI", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button16.Location = New System.Drawing.Point(116, 211)
+        Me.Button16.Name = "Button16"
+        Me.Button16.Size = New System.Drawing.Size(50, 50)
+        Me.Button16.TabIndex = 12
+        Me.Button16.UseVisualStyleBackColor = False
+        '
+        'Button17
+        '
+        Me.Button17.BackColor = System.Drawing.Color.White
+        Me.Button17.Font = New System.Drawing.Font("Microsoft JhengHei UI", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Button17.ForeColor = System.Drawing.Color.Pink
+        Me.Button17.Location = New System.Drawing.Point(257, 285)
+        Me.Button17.Name = "Button17"
+        Me.Button17.Size = New System.Drawing.Size(50, 23)
+        Me.Button17.TabIndex = 16
+        Me.Button17.Text = "Reset"
+        Me.Button17.UseVisualStyleBackColor = False
+        '
+        'frmUnoAssist
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Pink
+        Me.ClientSize = New System.Drawing.Size(331, 330)
+        Me.Controls.Add(Me.Button17)
+        Me.Controls.Add(Me.Button13)
+        Me.Controls.Add(Me.Button14)
+        Me.Controls.Add(Me.Button15)
+        Me.Controls.Add(Me.Button16)
+        Me.Controls.Add(Me.Button9)
+        Me.Controls.Add(Me.Button10)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.Button12)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Button6)
+        Me.Controls.Add(Me.Button7)
+        Me.Controls.Add(Me.Button8)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "frmUnoAssist"
+        Me.Text = "Uno 小助手"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents Button13 As Button
+    Friend WithEvents Button14 As Button
+    Friend WithEvents Button15 As Button
+    Friend WithEvents Button16 As Button
+    Friend WithEvents Button17 As Button
+End Class
---
+++ Uno Assist/Form1.vb
@@ -0,0 +1,203 @@
+﻿Public Class frmUnoAssist
+    Private Sub frmUnoAssist_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.BackColor = Color.Pink Then
+            Button1.BackColor = Color.Red
+            Button1.Text = "✕"
+        Else
+            Button1.BackColor = Color.Pink
+            Button1.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If Button8.BackColor = Color.Pink Then
+            Button8.BackColor = Color.Red
+            Button8.Text = "✕"
+        Else
+            Button8.BackColor = Color.Pink
+            Button8.Text = ""
+        End If
+    End Sub
+    Private Sub Button16_Click(sender As Object, e As EventArgs) Handles Button16.Click
+        If Button16.BackColor = Color.Pink Then
+            Button16.BackColor = Color.Red
+            Button16.Text = "✕"
+        Else
+            Button16.BackColor = Color.Pink
+            Button16.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        If Button12.BackColor = Color.Pink Then
+            Button12.BackColor = Color.Red
+            Button12.Text = "✕"
+        Else
+            Button12.BackColor = Color.Pink
+            Button12.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If Button2.BackColor = Color.LightYellow Then
+            Button2.BackColor = Color.Yellow
+            Button2.Text = "✕"
+        Else
+            Button2.BackColor = Color.LightYellow
+            Button2.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        If Button7.BackColor = Color.LightYellow Then
+            Button7.BackColor = Color.Yellow
+            Button7.Text = "✕"
+        Else
+            Button7.BackColor = Color.LightYellow
+            Button7.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button15_Click(sender As Object, e As EventArgs) Handles Button15.Click
+        If Button15.BackColor = Color.LightYellow Then
+            Button15.BackColor = Color.Yellow
+            Button15.Text = "✕"
+        Else
+            Button15.BackColor = Color.LightYellow
+            Button15.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        If Button11.BackColor = Color.LightYellow Then
+            Button11.BackColor = Color.Yellow
+            Button11.Text = "✕"
+        Else
+            Button11.BackColor = Color.LightYellow
+            Button11.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        If Button6.BackColor = Color.LightCyan Then
+            Button6.BackColor = Color.Blue
+            Button6.Text = "✕"
+        Else
+            Button6.BackColor = Color.LightCyan
+            Button6.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If Button3.BackColor = Color.LightCyan Then
+            Button3.BackColor = Color.Blue
+            Button3.Text = "✕"
+        Else
+            Button3.BackColor = Color.LightCyan
+            Button3.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs) Handles Button14.Click
+        If Button14.BackColor = Color.LightCyan Then
+            Button14.BackColor = Color.Blue
+            Button14.Text = "✕"
+        Else
+            Button14.BackColor = Color.LightCyan
+            Button14.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        If Button10.BackColor = Color.LightCyan Then
+            Button10.BackColor = Color.Blue
+            Button10.Text = "✕"
+        Else
+            Button10.BackColor = Color.LightCyan
+            Button10.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        If Button5.BackColor = Color.Honeydew Then
+            Button5.BackColor = Color.Green
+            Button5.Text = "✕"
+        Else
+            Button5.BackColor = Color.Honeydew
+            Button5.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs) Handles Button13.Click
+        If Button13.BackColor = Color.Honeydew Then
+            Button13.BackColor = Color.Green
+            Button13.Text = "✕"
+        Else
+            Button13.BackColor = Color.Honeydew
+            Button13.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        If Button4.BackColor = Color.Honeydew Then
+            Button4.BackColor = Color.Green
+            Button4.Text = "✕"
+        Else
+            Button4.BackColor = Color.Honeydew
+            Button4.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        If Button9.BackColor = Color.Honeydew Then
+            Button9.BackColor = Color.Green
+            Button9.Text = "✕"
+        Else
+            Button9.BackColor = Color.Honeydew
+            Button9.Text = ""
+        End If
+    End Sub
+
+    Private Sub Button17_Click(sender As Object, e As EventArgs) Handles Button17.Click
+        Button1.BackColor = Color.Pink
+        Button8.BackColor = Color.Pink
+        Button12.BackColor = Color.Pink
+        Button16.BackColor = Color.Pink
+
+        Button2.BackColor = Color.LightYellow
+        Button7.BackColor = Color.LightYellow
+        Button11.BackColor = Color.LightYellow
+        Button15.BackColor = Color.LightYellow
+
+        Button4.BackColor = Color.Honeydew
+        Button5.BackColor = Color.Honeydew
+        Button9.BackColor = Color.Honeydew
+        Button13.BackColor = Color.Honeydew
+
+        Button3.BackColor = Color.LightCyan
+        Button6.BackColor = Color.LightCyan
+        Button10.BackColor = Color.LightCyan
+        Button14.BackColor = Color.LightCyan
+
+        Button1.Text = ""
+        Button2.Text = ""
+        Button3.Text = ""
+        Button4.Text = ""
+        Button5.Text = ""
+        Button6.Text = ""
+        Button7.Text = ""
+        Button8.Text = ""
+        Button9.Text = ""
+        Button10.Text = ""
+        Button11.Text = ""
+        Button12.Text = ""
+        Button13.Text = ""
+        Button14.Text = ""
+        Button15.Text = ""
+        Button16.Text = ""
+    End Sub
+
+End Class
